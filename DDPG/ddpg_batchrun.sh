732dd96f3a5ba756915e07b160b54056b8e7ac00
Victor Qin <vqin@holylogin03.rc.fas.harvard.edu>
2020-12-28 00:55:37 -0500

    small edit for batch working

--- DDPG/ddpg_batchrun.sh
+++ DDPG/ddpg_batchrun.sh
@@ -4,9 +4,9 @@
 #SBATCH -t 0-24:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p shared   # Partition to submit to
 #SBATCH --mem=8192           # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o batch_logs/myoutput_%j_%a.out  # File to which STDOUT will be written, %j inserts jobid, %a is the job array
-#SBATCH -e batch_logs/myerrors_%j_%a.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH -o ../batch_logs/myoutput_%j_%a.out  # File to which STDOUT will be written, %j inserts jobid, %a is the job array
+#SBATCH -e ../batch_logs/myerrors_%j_%a.err  # File to which STDERR will be written, %j inserts jobid
 
 module load Anaconda3/5.0.1-fasrc01 ; conda info --envs ;
-source activate es100
+source activate es100 ;
 python3 ddpg_avg.py
