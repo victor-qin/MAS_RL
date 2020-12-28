abfc6df5419ebf6dc49198f8169f731f797cf9b1
Victor Qin <vqin@holylogin03.rc.fas.harvard.edu>
2020-12-28 01:47:41 -0500

    edits to cores

--- DDPG/ddpg_batchrun.sh
+++ DDPG/ddpg_batchrun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -n 1                # Number of cores (-n)
+#SBATCH -n 3                # Number of cores (-n)
 #SBATCH -N 1               # Ensure that all cores are on one Node (-N)
 #SBATCH -t 0-24:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p shared   # Partition to submit to
@@ -8,5 +8,5 @@
 #SBATCH -e ../batch_logs/myerrors_%j_%a.err  # File to which STDERR will be written, %j inserts jobid
 
 module load Anaconda3/5.0.1-fasrc01 ; conda info --envs ;
-source activate es100 ;
+source activate es100
 python3 ddpg_avg.py
